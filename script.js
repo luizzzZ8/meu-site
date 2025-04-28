cc081ae54a626fe6e41c2da7eecef62f8bdce6c1
luizzzZ8 <luizgustavo.neves88@gmail.com>
2025-04-28 18:23:03 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function addToCart(productName, price) {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    cart.push({ name: productName, price: price });
-    localStorage.setItem('cart', JSON.stringify(cart));
-    alert(productName + " adicionado à sacola!");
-}
